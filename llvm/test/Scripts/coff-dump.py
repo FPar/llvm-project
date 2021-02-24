e8c216833008fc6980c872efb7c13cb6fb6f24e7
Fabian Parzefall <fabian.parzefall@mailbox.org>
2021-02-24 08:15:38 -0800

    Update shebangs to python2

--- llvm/test/Scripts/coff-dump.py
+++ llvm/test/Scripts/coff-dump.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #===-- coff-dump.py - COFF object file dump utility-------------------------===#
 #
 #                     The LLVM Compiler Infrastructure
--- llvm/utils/release/findRegressions-nightly.py
+++ llvm/utils/release/findRegressions-nightly.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import re, string, sys, os, time
 
 DEBUG = 0
